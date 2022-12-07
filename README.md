5554cf8aab07d38050bb9721844901275f8279f9
PasiturintisS <105636873+PasiturintisS@users.noreply.github.com>
2022-12-07 13:06:31 +0200

    3 files modified

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-zryahr)
 
+Parssing and using data with event binding
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent {
   serverCreationStatus = 'no server is created!';
+  serverName ='';
 
   onCreateServer(){
     this.serverCreationStatus = 'server was created.';
+  }
+
+  onUpdateServerStatus(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
