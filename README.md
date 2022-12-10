666ce13e478a31f90b39e7c8f0a3cb5195f21d11
PasiturintisS <105636873+PasiturintisS@users.noreply.github.com>
2022-12-10 16:54:19 +0200

    4 files modified

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-zryahr)
 
-37. Using ngIf to Output Data Conditionally
-38. Enhancing ngIf with an Else Condition
+39. Styling Elements Dynamically with ngStyle
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
 
+  // random skaiciai generuojami tiesiog kad nebutu visad statinis ID ir statusas servo zodziu kad butu idomiau
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    this.serverId = Math.floor(Math.random() * 100);
+  }
+
+  getServerStatus(): string {
+    return this.serverStatus;
+  }
+
+  getServerId(): number {
+    return this.serverId;
+  }
+
+  // nurodom kokia spalva bus
+  getColor(): string {
+    return this.serverStatus == 'online' ? 'green' : 'red';
+  }
 }
